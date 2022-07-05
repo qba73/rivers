714a3b4e5790f8c0c07f6dea569ec1b417742b3a
Jakub Jarosz <jakub@jarosz.dev>
2022-07-05 08:42:28 +0100

    Add CLI to data puller

--- puller.go
+++ puller.go
@@ -2,6 +2,7 @@ package rivers
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -10,32 +11,38 @@ import (
 
 type option func(*Puller) error
 
-func WithSaver(st Store) option {
+func WithInterval(duration string) option {
 	return func(p *Puller) error {
-		if st == nil {
-			return errors.New("nil data saver")
+		if duration == "" {
+			return errors.New("setting up pulling interval: nil duration")
 		}
-		p.Store = st
+		duration, err := time.ParseDuration(duration)
+		if err != nil {
+			return fmt.Errorf("setting up pulling interval: %w", err)
+		}
+		p.Interval = duration
 		return nil
 	}
 }
 
 type Puller struct {
-	Client   *Client
-	Store    Store
-	Interval time.Duration
+	Client      *Client
+	ReadingRepo *ReadingsRepo
+	Interval    time.Duration
 }
 
 func NewPuller(opts ...option) (*Puller, error) {
-	riverClient := NewClient()
-	store, err := NewSQLiteStore("data.db")
+	client := NewClient()
+	store, err := NewSQLiteStore("waterlevels.db")
 	if err != nil {
 		return nil, fmt.Errorf("%w: creating data puller", err)
 	}
+	repo := OpenReadingsRepo(store)
+
 	p := Puller{
-		Client:   riverClient,
-		Store:    store,
-		Interval: 5 * time.Minute,
+		Client:      client,
+		ReadingRepo: repo,
+		Interval:    5 * time.Minute,
 	}
 
 	for _, opt := range opts {
@@ -48,13 +55,12 @@ func NewPuller(opts ...option) (*Puller, error) {
 }
 
 func (p *Puller) Run() error {
-	// Retrive new sensor data
 	stationReadings, err := p.Client.GetLatestWaterLevels()
 	if err != nil {
 		return err
 	}
 	for _, reading := range stationReadings {
-		err = p.Store.Save(reading)
+		err = p.ReadingRepo.Add(reading)
 		if err != nil {
 			return err
 		}
@@ -65,24 +71,47 @@ func (p *Puller) Run() error {
 // RunPuller holds all required machinery
 // to run the river data puller.
 func RunPuller() {
+	fset := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	interval := flag.String("interval", "5m", "data puling interval, example: 5m, 30m, 1h")
+	help := flag.Bool("h", false, "show usage and exit")
+	if err := fset.Parse(os.Args[1:]); err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	if *help {
+		fmt.Fprint(os.Stdout, pullerUsage)
+	}
+
 	log := log.New(os.Stdout, "PULLER : ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
-
-	p, err := NewPuller()
-	if err != nil {
-		panic(err)
-	}
-	duration, err := time.ParseDuration("1m")
+	p, err := NewPuller(
+		WithInterval(*interval),
+	)
 	if err != nil {
 		panic(err)
 	}
 
-	log.Println("rivers: Start pulling sensor data")
+	log.Println("puller: Start pulling sensor data")
 
 	for range time.NewTicker(p.Interval).C {
-		log.Println("rivers: Pulling data")
+		log.Println("puller: Pulling data")
 		if err := p.Run(); err != nil {
 			break
 		}
-		log.Printf("rivers: Data saved. Resuming in %s", duration)
+		log.Printf("puller: Data saved. Resuming in %s", *interval)
 	}
 }
+
+var pullerUsage = `
+waterlevel - water levels sensor data collector.
+
+Flags:
+-h            "Show help"
+-interval     "Pulling data interval, example: 1m, 5m, 1h"
+
+Examples:
+	// Start puller and collect data every 5 minutes (default settings)
+	waterlevel
+	// Start puller and collect data every hour
+	waterlevel -interval 1h
+`
